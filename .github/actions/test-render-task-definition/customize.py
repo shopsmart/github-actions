4ad54b51cc269ebf7f35abd089e58c4705fd7cab
Mike Carey <32496966+mike-carey@users.noreply.github.com>
2023-02-21 10:50:27 -0600

    Removes dead code

--- .github/actions/test-render-task-definition/customize.py
+++ .github/actions/test-render-task-definition/customize.py
@@ -5,18 +5,6 @@
 :see: https://github.com/kolypto/j2cli#customization
 """
 
-import yaml
-import os
-
-# Loader
-try:
-    # PyYAML 5.1 supports FullLoader
-    Loader = yaml.FullLoader
-except AttributeError:
-    # Have to use SafeLoader for older versions
-    Loader = yaml.SafeLoader
-
-__DIR__ = os.path.dirname(os.path.realpath(__file__))
 
 
 def alter_context(context):
