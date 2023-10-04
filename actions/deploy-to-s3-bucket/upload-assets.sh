fa785b98ac4911e9d9a591af94dd0113c6d069c0
Mike Carey <mike-carey@users.noreply.github.com>
2023-10-04 17:02:53 -0500

    Revert "Refactors deploy-to-s3-bucket action to allow a single file to be uploaded (#34)"
    This reverts commit 47339abd883865524de5070f2956a85184feec00.

--- actions/deploy-to-s3-bucket/upload-assets.sh
+++ actions/deploy-to-s3-bucket/upload-assets.sh
@@ -6,31 +6,10 @@ function upload-assets() {
   local path="$1"
 
   local s3_path="$S3_BUCKET"
-  if [ -n "${S3_BUCKET_PATH:-}" ] && [ "${s3_path:0:1}" != / ]; then
-    s3_path+="/$S3_BUCKET_PATH"
-  fi
+  [ -z "${S3_BUCKET_PATH:-}" ] || s3_path+="/$S3_BUCKET_PATH"
 
-  local options=()
-
-  if [ -d "$path" ]; then
-    echo "[DEBUG] $path is a directory" >&2
-
-    [ "${path: -1}" = /    ] || path="$path/"
-    [ "${s3_path: -1}" = / ] || s3_path="$s3_path/"
-
-    options+=(--recursive)
-  elif [ -f "$path" ]; then
-    echo "[DEBUG] $path is a file" >&2
-    [ -n "${S3_BUCKET_PATH:-}" ] || s3_path="$s3_path/"
-  else
-    echo "[ERROR] Could not find a file or directory for $path" >&2
-    return 1
-  fi
-
-  echo "[DEBUG] Copying $path to s3://$s3_path" >&2
-  # shellcheck disable=SC2068
-  # We want the options to expand
-  aws s3 cp ${options[@]} "$path" "s3://$s3_path"
+  echo "[DEBUG] Copying $path/ to s3://$s3_path/" >&2
+  aws s3 cp --recursive "$path/" "s3://$s3_path/"
 }
 
 if [ "${BASH_SOURCE[0]}" = "$0" ]; then
