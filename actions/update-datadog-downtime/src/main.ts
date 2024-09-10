3ef8bfd4b31b1be5825c95f0c999eec88dbcdbd1
Mike Carey <32496966+mike-carey@users.noreply.github.com>
2024-09-10 10:23:01 -0500

    Adds an action to update a datadog downtime
    This first commit gets things setup and adds a time parse function

---
+++ actions/update-datadog-downtime/src/main.ts
@@ -0,0 +1,7 @@
+/**
+ * An action to update a Datadog downtime.
+ */
+
+async function run() {
+  throw new Error("unimplemented")
+}
---
+++ actions/update-datadog-downtime/src/time.ts
@@ -0,0 +1,34 @@
+import moment from 'moment'
+
+// We accept a custom format to add time from now, such as: 4h 5m 30s
+const CustomTimeRegex = /^([0-9]+\ *h\ *)?([0-9]+\ *m\ *)?([0-9]+\ *s\ *)?$/
+
+/**
+ * Parses out our custom format or sends back what was provided
+ *
+ * @param now The time to add to.  This is passed in as a parameter for testing and also to make sure that both start and end times are taken from the same time.
+ * @param provided The provided time string.  If not in the custom format, it should be an ISO-8601 format, but this function will not check.
+ * @returns The datetime in ISO-8601 format.  If the provided datetime does not match the custom format, it will return what was provided
+ */
+export function parseTime(now: moment.Moment, provided: string|undefined): string|null {
+  if (!provided) {
+    return null
+  }
+
+  const matches = provided.match(CustomTimeRegex)
+  // 0 = the whole match (since everything is optional)
+  // 1 = hours
+  // 2 = minutes
+  // 3 = seconds
+  if (provided == '' || matches == null || matches[0] == '') {
+    return provided
+  }
+
+  // If any of the matches are undefined, moment adds nothing
+  return now
+    .add(matches[0], 'hours')
+    .add(matches[1], 'minutes')
+    .add(matches[2], 'seconds')
+    .toDate()
+    .toISOString()
+}
