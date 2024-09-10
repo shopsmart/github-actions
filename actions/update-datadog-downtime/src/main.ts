5fde048fd2c4e7d40e0f4a725d21624d47ccac7b
Mike Carey <32496966+mike-carey@users.noreply.github.com>
2024-09-10 17:27:43 -0500

    Adds everything to main

--- actions/update-datadog-downtime/src/main.ts
+++ actions/update-datadog-downtime/src/main.ts
@@ -2,6 +2,34 @@
  * An action to update a Datadog downtime.
  */
 
+import * as core from '@actions/core'
+import * as httpm from '@actions/http-client'
+import { Config } from './config'
+
+const DowntimeURL = "https://api.datadoghq.com/api/v2/downtime"
+const Headers = {
+  ApiKey: "DD-API-KEY",
+  AppKey: "DD-APP-KEY",
+}
+
+/**
+ * The main runtime
+ */
 async function run() {
-  throw new Error("unimplemented")
+  const config = new Config()
+
+  const req = new httpm.HttpClient()
+  req.requestOptions = {
+    headers: {
+      [httpm.Headers.ContentType]: httpm.MediaTypes.ApplicationJson,
+      [httpm.Headers.Accept]: httpm.MediaTypes.ApplicationJson,
+      [Headers.ApiKey]: config.apiKey,
+      [Headers.AppKey]: config.appKey,
+    }
+  }
+
+  const resp = await req.post(DowntimeURL, config.payload())
+
+  core.setOutput("status-code", resp.message.statusCode)
+  core.setOutput("body", await resp.readBody())
 }
